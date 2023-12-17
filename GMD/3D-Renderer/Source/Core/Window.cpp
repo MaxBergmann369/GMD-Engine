42bae2332f62bd397e3df0feb71bb00a19753678
jkatsanis <katsanis18@gmail.com>
2023-12-17 18:45:54 +0100

    Can compile in release

--- GMD/3D-Renderer/Source/Core/Window.cpp
+++ GMD/3D-Renderer/Source/Core/Window.cpp
@@ -48,43 +48,18 @@ gmd::Window::~Window()
 
 // Public
 
-void gmd::Window::Update()
+void gmd::Window::EndFrame()
 {
-    glfwPollEvents();
-
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-
-    // Imgui
-
-           // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-    {
-        static float f = 0.0f;
-        static int counter = 0;
-
-        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-
-        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-
-        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-            counter++;
-        ImGui::SameLine();
-        ImGui::Text("counter = %d", counter);
-
-        ImGui::End();
-    }
-    
-
     ImGui::Render();
-    int display_w, display_h;
-    glfwGetFramebufferSize(this->m_Window, &display_w, &display_h);
-    glViewport(0, 0, display_w, display_h);
-    glClear(GL_COLOR_BUFFER_BIT);
-
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
     glfwSwapBuffers(this->m_Window);
+}
+
+void gmd::Window::NewFrame()
+{
+    glfwPollEvents();
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
 }
---
+++ GMD/README.md
@@ -0,0 +1,11 @@
+# GLFW_GLEW_IMGUI
+A glfw, glew, imgui template all setuped following a core-app structure 
+
+## How to build?
+
+First of all make sure to navigate in the Root `CMakeLists.txt ` file and rename the variables as you wish. It's important to rename the folders aswell!. (They are on line 1-3). Sorry i was to lazy to create a setup file.
+
+- Create a `Build` dir
+- Make sure to have CMake installed
+- Navigate in the `Build` dir
+- Run `cmake ..`
